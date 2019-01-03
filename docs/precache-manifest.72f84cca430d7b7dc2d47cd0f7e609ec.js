7da357867159c9feb1093d039005bc6e34734c9a
Juan <jupegarnica@gmail.com>
2019-01-03 13:35:14 +0100

    publish to gh-pages docs

---
+++ docs/precache-manifest.72f84cca430d7b7dc2d47cd0f7e609ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90ba072e34f32270a5a3",
+    "url": "/counter-react/static/js/runtime~main.90ba072e.js"
+  },
+  {
+    "revision": "a77970884511a2be5bc4",
+    "url": "/counter-react/static/js/main.a7797088.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/counter-react/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "a77970884511a2be5bc4",
+    "url": "/counter-react/static/css/main.c1cdda41.chunk.css"
+  },
+  {
+    "revision": "a0066e0d6a66a8b2cca29128efbeabd9",
+    "url": "/counter-react/index.html"
+  }
+];
